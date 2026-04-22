9ba79e25eb5022ab9d8ab09668df1e001abe4a7b
GabrielCalisaya <gabrielcalisaya98@gmail.com>
2026-04-22 20:15:57 -0300

    Realizado los cambios en Precios agregando el hover dinamico, y Tooltops Bootstrap

---
+++ js/precios.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+    // Inicializar tooltips de Bootstrap
+    var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+    var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+        return new bootstrap.Tooltip(tooltipTriggerEl)
+    });
+    //hover dinamico
+    $('#tabla-precios tbody tr').on('mouseenter', function() {
+        $(this).addClass('table-active');
+    }).on('mouseleave', function() {
+        $(this).removeClass('table-active');
+    });
+
+    console.log("Tarea4: tooltips y hover dinámico funcionando correctamente.");
+});
