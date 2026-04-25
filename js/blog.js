af7051f35608fa6283c82cccc4fb09d539e8d560
Gutierrez Nanda, Mauro Nahuel <39233689@fi.unju.edu.ar>
2026-04-25 16:55:39 -0300

    Arregla botón de comentarios

--- js/blog.js
+++ js/blog.js
@@ -12,29 +12,35 @@ $(document).ready(function(){
     });
 
     // 2. Botón Ver más / Ver menos
-    // Primero, ocultamos los comentarios al cargar
-    $(".comentario-item").not(":first").hide();
+    $(".comentario-item").not(":first").addClass("comentario-extra").hide();
 
     $("#btn-comentarios").click(function(){
-        var extras = $(".comentario-item").not(":first");
-        extras.slideToggle(500);
+        // Al terminar el slideToggle, corremos la función de scroll para que revelen suavemente
+        $(".comentario-extra").slideToggle(500, function() {
+            checkReveal(); 
+        });
         
-        if ($(this).text().trim() === "Ver más comentarios") {
-            $(this).text("Ver menos");
-        } else {
-            $(this).text("Ver más comentarios");
-        }
+        var $btn = $(this);
+        setTimeout(function() {
+            var abierto = $(".comentario-extra").is(":visible");
+            $btn.text(abierto ? "Ver menos" : "Ver más comentarios");
+        }, 50);
     });
 
-    // 3. Animación de scroll
-    $(window).scroll(function() {
+    // 3. Animación de scroll limpia (Usando clases CSS)
+    function checkReveal() {
         $('.reveal').each(function() {
-            var top_of_element = $(this).offset().top;
-            var bottom_of_window = $(window).scrollTop() + $(window).height();
-            if (bottom_of_window > top_of_element) {
-                $(this).animate({'opacity':'1'}, 1000);
+            // Solo procesamos elementos que no estén ocultos por display:none
+            if ($(this).is(':visible')) {
+                var top_of_element = $(this).offset().top;
+                var bottom_of_window = $(window).scrollTop() + $(window).height();
+                if (bottom_of_window > top_of_element - 50) { // -50px de margen para que se vea antes
+                    $(this).addClass('active');
+                }
             }
         });
-    });
+    }
 
+    $(window).scroll(checkReveal);
+    checkReveal(); // Ejecutar al cargar la página por primera vez
 });
